d925352aaad805c9c1d79a6f42a381337cf8a7a5
amitsingh-007 <amit.singh@carwale.com>
2021-07-11 10:44:40 +0530

    added pwa event listeners

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,17 @@
 import "../styles/globals.css";
+import { pwaTrackingListeners } from "../scripts/pwaEventlisteners";
+
+const isBrowser = typeof window !== "undefined";
+
+if (isBrowser) {
+  pwaTrackingListeners();
+}
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
 
-if (typeof window !== "undefined" && "serviceWorker" in navigator) {
+if (isBrowser && "serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
       .register("/service-worker.js")
---
+++ scripts/pwaEventlisteners.js
@@ -0,0 +1,38 @@
+const fireTracking = (label) => {
+  //Fire tracking here
+  console.log(label);
+};
+
+export const pwaTrackingListeners = () => {
+  const fireAddToHomeScreenImpression = (event) => {
+    fireTracking("Add to homescreen shown");
+    //will not work for chrome, untill fixed
+    event.userChoice.then((choiceResult) => {
+      fireTracking(`User clicked ${choiceResult}`);
+    });
+    //This is to prevent `beforeinstallprompt` event that triggers again on `Add` or `Cancel` click
+    window.removeEventListener(
+      "beforeinstallprompt",
+      fireAddToHomeScreenImpression
+    );
+  };
+  window.addEventListener("beforeinstallprompt", fireAddToHomeScreenImpression);
+
+  //Track web app install by user
+  window.addEventListener("appinstalled", (event) => {
+    fireTracking("PWA app installed by user!!! Hurray");
+  });
+
+  //Track from where your web app has been opened/browsed
+  window.addEventListener("load", () => {
+    let trackText;
+    if (navigator && navigator.standalone) {
+      trackText = "Launched: Installed (iOS)";
+    } else if (matchMedia("(display-mode: standalone)").matches) {
+      trackText = "Launched: Installed";
+    } else {
+      trackText = "Launched: Browser Tab";
+    }
+    fireTracking(trackText);
+  });
+};
